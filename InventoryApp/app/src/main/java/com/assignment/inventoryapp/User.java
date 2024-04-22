057907682b728aa1523adbf7b80989c36363b21b
LaishaR <126051005+LaishaR@users.noreply.github.com>
2024-04-21 20:52:13 -0500

    Delete InventoryApp directory

--- InventoryApp/app/src/main/java/com/assignment/inventoryapp/User.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.inventoryapp;
-
-public class User {
-    private int id;
-    private String username;
-    private String password;
-
-    // Constructor, getters, and setters
-}
